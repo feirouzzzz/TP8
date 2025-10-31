4b4eabcd4e271da66ae8171d10185faf646d5b03
feirouzzzz <feirouzelmouhtadi@gmail.com>
2025-10-31 21:16:18 +0100

    second

---
+++ Tp8/src/main/java/com/example/Repositories/CompteRepository.java
@@ -0,0 +1,10 @@
+
+package com.example.Repositories;
+
+import com.example.entities.Compte;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
---
+++ Tp8/src/main/java/com/example/entities/Compte.java
@@ -0,0 +1,35 @@
+package com.example.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Compte {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double solde;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+
+    @Enumerated(EnumType.STRING)
+    private TypeCompte type;
+}
