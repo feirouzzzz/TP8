85168b6dc357ba079844fa27faeb6ade6cd06fdf
feirouzzzz <feirouzelmouhtadi@gmail.com>
2025-11-15 09:26:49 +0100

    r

--- Tp8/src/main/java/com/example/Repositories/CompteRepository.java
+++ Tp8/src/main/java/com/example/Repositories/CompteRepository.java
@@ -1,9 +1,8 @@
-
 package com.example.Repositories;
 
-import com.example.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.example.entities.Compte;
 
 @Repository
 public interface CompteRepository extends JpaRepository<Compte, Long> {
